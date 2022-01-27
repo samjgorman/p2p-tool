908312009222d394d320c8e54a15737afd77cb22
samjgorman <samjgorman@gmail.com>
2022-01-26 20:21:49 -0800

    Command line prompt for https or http, and error handling

--- src/peer_discovery_modules/mdns/mdns.ts
+++ src/peer_discovery_modules/mdns/mdns.ts
@@ -1,43 +1,86 @@
+import { Console } from "console";
 
 
 // import the module
 const mdns = require("mdns");
 
-// advertise a http server on port 4321
-const ad = mdns.createAdvertisement(mdns.tcp("https"), 4321);
-ad.start();
-
-// watch all http servers
-const browser = mdns.createBrowser(mdns.tcp("https"));
-// const browser = mdns.createBrowser(mdns.tcp("http"));
-
-//Duplicates exist in serviceUp, so use a set 
-var available_service_table: Array<any>;
+var inquirer = require('inquirer');
 
 
-//A matching service appeared
-browser.on("serviceUp", (service) => {
-  //Can convert this service to an array
-  var service_arr = service.toArray();
-  available_service_table.push(service_arr);
-  // console.log("service up: ", service);
-});
 
-//A matching service disappeared
-browser.on("serviceDown", (service) => {
-  // const idx_to_remove = available_service_table.indexOf(service);
-  // available_service_table.splice(idx_to_remove,1);
+function advertiseServer(protocol:string){
+  //advertise a http server on port 4321
+  const ad = mdns.createAdvertisement(mdns.tcp(protocol), 4321);
+  ad.start();
+}
 
-  // console.log("service down: ", service);
-});
+function discoverServices(protocol:string){
+
+  advertiseServer(protocol);  // watch all http servers
+  const browser = mdns.createBrowser(mdns.tcp(protocol));
+  // const browser = mdns.createBrowser(mdns.tcp("http"));
+  
+  //Duplicates exist in serviceUp, so use a set 
+  
+  //A matching service appeared
+  browser.on("serviceUp", (service) => {
+    //Can convert this service to an array
+    var name = service.name;
+    console.log("Discovered device name: ",name);
+  });
+  
+  //A matching service disappeared
+  browser.on("serviceDown", (service) => {
+   
+    console.log("service down: ", service.name);
+  });
+  
+  //There was an error
+  browser.on("error", (exception) => {
+    console.log("error", exception);
+  });
+
+  browser.start();
+ 
+}
+
+function discoverDriver(){
+
+  const service = inquirer
+    .prompt([{
+      /* Pass your questions in here */
+      type: "list",
+			name: "protocol",
+      message: 'What service would you like to use for MDNS discovery?',
+      choices: ['http', 'https']
+    }
+    ])
+    .then((answer) => {
+      console.log(     answer.protocol );
+      discoverServices( answer.protocol );
+
+    })
+    .catch((error) => {
+      if (error.isTtyError) {
+        // Prompt couldn't be rendered in the current environment
+        console.log("There's been an error");
+      } else {
+        // Something else went wrong
+        console.log("There's been a different error");
+        console.log(error);
+
+      }
+    });
+
+
+
+}
+
+discoverDriver();
+
+
 
 //call this after the two listeners have finished...
-
-
-browser.start();
-
-
-
 
 // discover all available service types
 // const all_the_types = mdns.browseThemAll(); // all_the_types is just another browser...
