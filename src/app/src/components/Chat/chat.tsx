25e3c4d2ea737898a0423a342bc95db7dfb28bfe
samjgorman <samjgorman@gmail.com>
2022-02-15 15:17:13 -0800

    Documentation, formatting, refactor client code

--- src/app/src/components/Chat/chat.tsx
+++ src/app/src/components/Chat/chat.tsx
@@ -1,71 +1,58 @@
 import React, { useEffect, useState } from "react";
 import ChatHistory from "../ChatHistory/chatHistory";
-import ChatInput from "../ChatInput/input";
+import ChatInput from "../ChatInput/chatInput";
 import ChatMessage from "../ChatMessage/message";
 
-
 /**
- * This component renders a list of live chat messages to be displayed
- * on the web page for the current video.
- * @param props is an object that contains these properties
- *    videoId - id of the YouTube video for which we should get comments from
- *    player - a handle on the player for the video being played
+ * Stateful container that...
+ * 1. Renders the ChatHistory, ChatInput, and ChatMessage components
+ * 2. Listens for message objects the client has sent or received,
+ *    and renders them
  */
- function Chat(props) {
-   
-    const [stackPopulated, setStackPopulated] = useState(false);
-    const [messagesToRenderStack, setMessagesToRenderStack] = useState([]);
-    useEffect(() => {
-      //Listen for submitted messages
-      window.Main.on("i_submitted_message", (event,arg) =>{
-          console.log("Chat object received")
-          console.log(event) 
-          const messageObjToRender = JSON.parse(event)
-          const newState = [...messagesToRenderStack, messageObjToRender];
-          setMessagesToRenderStack(newState)
-          setStackPopulated(true)
-
-      })
-
-      window.Main.on("peer_submitted_message", (event,arg) =>{
-        console.log("Chat object received from peer")
-        console.log(event) 
-        const messageObjToRender = JSON.parse(event)
-        const newState = [...messagesToRenderStack, messageObjToRender];
-        setMessagesToRenderStack(newState)
-        setStackPopulated(true)
-    })
-
-    return function cleanup() {
-        window.Main.removeAllListeners("client_submitted_message")
-        window.Main.removeAllListeners("peer_submitted_message")
-
-        
-       };
-
+function Chat() {
+  const [stackPopulated, setStackPopulated] = useState(false);
+  const [messagesToRenderStack, setMessagesToRenderStack] = useState([]);
+  useEffect(() => {
+    //Listen for messages the client has sent
+    window.Main.on("i_submitted_message", (event, arg) => {
+      console.log("Chat object received");
+      console.log(event);
+      const messageObjToRender = JSON.parse(event);
+      const newState = [...messagesToRenderStack, messageObjToRender];
+      setMessagesToRenderStack(newState);
+      setStackPopulated(true);
+    });
+    //Listen for messages the client has received
+    window.Main.on("peer_submitted_message", (event, arg) => {
+      console.log("Chat object received from peer");
+      console.log(event);
+      const messageObjToRender = JSON.parse(event);
+      const newState = [...messagesToRenderStack, messageObjToRender];
+      setMessagesToRenderStack(newState);
+      setStackPopulated(true);
     });
 
-    //TODO: Add a type here...
-  
-    return (
-      <div className="LiveChat">
+    return function cleanup() {
+      window.Main.removeAllListeners("client_submitted_message");
+      window.Main.removeAllListeners("peer_submitted_message");
+    };
+  });
 
-          <ChatHistory/> 
-          {stackPopulated &&
-           messagesToRenderStack.map((chatMessage, i) => (
-              <ChatMessage
-                key={i}
-                chatMessage={chatMessage.message}
-                timestamp={chatMessage.timestamp}
-                sender= {chatMessage.sender}
-              />
-            ))
-          }
+  return (
+    <div className="LiveChat">
+      <ChatHistory />
+      {stackPopulated &&
+        messagesToRenderStack.map((chatMessage, i) => (
+          <ChatMessage
+            key={i}
+            chatMessage={chatMessage.message}
+            timestamp={chatMessage.timestamp}
+            sender={chatMessage.sender}
+          />
+        ))}
+      <ChatInput></ChatInput>
+    </div>
+  );
+}
 
-          <ChatInput></ChatInput>
-      </div>
-    );
-  }
-  
-  export default Chat;
-  
+export default Chat;
--- src/app/src/components/Friends/friendsList.tsx
+++ src/app/src/components/Friends/friendsList.tsx
@@ -1,4 +1,3 @@
-
 import React, { useEffect, useState } from "react";
 
 type FriendMetadata = {
@@ -6,107 +5,93 @@ type FriendMetadata = {
   lastSeen: string;
 };
 
-function FriendObject(props){
-
-  function handleFriend(friend:string){
-    //TODO: Request the local chat file for this friend and render it
-    // const friendName = friend[0].toString()
-    window.Main.getFriendChatObject(friend) //TODO: verify this
-
+/**
+ * Simple functional component to render a friend object,
+ * called in FriendsList
+ * @param props
+ * name: string, name of selected friend
+ * lastSeen: string, UNIX timestring representing time friend last seen
+ */
+function FriendObject(props) {
+  //Handler to pass selected friendName to Electron,
+  //used to render ChatHistory
+  function handleFriend(friend: string) {
+    window.Main.getFriendChatObject(friend);
   }
+  const lastSeenTimestamp: number = parseInt(props.lastSeen);
+  const readableLastSeen = new Date(lastSeenTimestamp).toLocaleTimeString(
+    "en-US"
+  );
 
-  const lastSeenTimestamp:number = parseInt(props.lastSeen)
-  const readableLastSeen = new Date(lastSeenTimestamp).toLocaleTimeString("en-US")
-  //TODO: If empty, state that user is offline
-
-    return (
-      <div className="FriendobjectContainer" onClick={()=> handleFriend(props.name)}
-> 
-        <div className="FriendObject">{props.name}</div>
-        <div className="FriendObject">{props.lastSeen === "" ? "Offline" : readableLastSeen}</div> 
-        </div>
-    );
+  return (
+    <div
+      className="FriendObjectContainer"
+      onClick={() => handleFriend(props.name)}
+    >
+      <div className="FriendObjectName">{props.name}</div>
+      <div className="FriendObjectLastSeen">
+        {props.lastSeen === "" ? "Offline" : readableLastSeen}
+      </div>
+    </div>
+  );
 }
 
 /**
- * This component renders a list of a user's friends,
- * and displays whether they are online or not. 
- * @param props is an object that contains these properties
- *    LiveChatMessage - an object that contains these properties
- *      timestamp - a Date object representing when the message was sent
- *      text - a string containing the message's text
- *      time_posted - an object containing these properties
- *          seconds - time posted in UNIX timestamp seconds
+ * Stateful container that renders a list of a user's friends,
+ * and displays whether they are online or not.
  */
+function FriendsList() {
+  const [friends, setFriends] = useState([]);
+  const [friendsPopulated, setFriendsPopulated] = useState(false);
 
- function FriendsList(props) { //refactor for TS 
-    const [friends, setFriends] = useState([]);
-    const [friendsPopulated, setFriendsPopulated] = useState(false);
-
-    function convertRecordToArray(friends: Record<string, FriendMetadata>):Array<Array<string>>{
-      //https://stackoverflow.com/questions/61350184/how-to-use-foreach-on-recordstring-object-in-typescript-and-angular
-      const friendsArr:Array<Array<string>> = []
-      for (const key in friends) {
-          const friendObject = []
-            // console.log(key)
-              friendObject.push(key)
-            for (const key2 in friends[key]) {
-                // console.log(friends[key][key2]);
-                  friendObject.push(friends[key][key2])  
-           
-            }
-          
-          friendsArr.push(friendObject)
-        }
-
-        return friendsArr
-
+  /**
+   * Utility function that converts a Record<string, FriendMetadata> to
+   * an Array<object>
+   * @param friends
+   * @returns friendsArr: Array<Array<string>> with schema
+   * [name:string, publicKey:string, lastSeen:string]
+   *  https://stackoverflow.com/questions/61350184/how-to-use-foreach-on-recordstring-object-in-typescript-and-angular
+   */
+  function convertRecordToArray(
+    friends: Record<string, FriendMetadata>
+  ): Array<Array<string>> {
+    const friendsArr: Array<Array<string>> = [];
+    for (const key in friends) {
+      const friendObject = [];
+      friendObject.push(key);
+      for (const key2 in friends[key]) {
+        friendObject.push(friends[key][key2]);
+      }
+      friendsArr.push(friendObject);
     }
-
-
-
-    //TODO: function that reads from the files/friends directory
-    //And presents a list of friends 
-    useEffect( () =>{
-        console.log("rendered ran")
-
-        //Run this every 5 seconds
-        //Listen to the route to get all friends for the given user
-        window.Main.on("get_all_friends_of_user", (event,arg) =>{
-            console.log("Friends object received")
-            console.log(event) 
-            const friendsRecord = event  //Typecheck this as a Record<string, FriendMetadata>
-
-            const friendsArray = convertRecordToArray(friendsRecord)
-            console.log(friendsArray) 
-            setFriends(friendsArray)
-            setFriendsPopulated(true)
-        })
-
-        return function cleanup() {
-            window.Main.removeAllListeners("get_all_friends_of_user")
-            // clearInterval(interval);
-
-           };
-        
-    }) //Fire once?
-
-
-    return (
-      <div className="FriendsListContainer">
-
-           {friendsPopulated && friends.map((friend: Record<string,FriendMetadata>, i) => (
-              <FriendObject
-                key={i}
-                name = {friend[0]}
-                lastSeen = {friend[2]}
-
-              />
-            ))
-           }
-        
-
-      </div>
-    );
+    return friendsArr;
   }
-  export default FriendsList;
+
+  useEffect(() => {
+    console.log("FriendsList rendered");
+    //Listen to the route to get all friends for the given user
+    window.Main.on("get_all_friends_of_user", (event, arg) => {
+      console.log("Friends object received");
+      console.log(event);
+      const friendsRecord = event; //Typecheck this as a Record<string, FriendMetadata>
+      const friendsArray = convertRecordToArray(friendsRecord);
+      setFriends(friendsArray);
+      setFriendsPopulated(true);
+      //TODO: write test to verify friendsArray renders correctly
+    });
+
+    return function cleanup() {
+      window.Main.removeAllListeners("get_all_friends_of_user");
+    };
+  });
+
+  return (
+    <div className="FriendsListContainer">
+      {friendsPopulated &&
+        friends.map((friend: Record<string, FriendMetadata>, i) => (
+          <FriendObject key={i} name={friend[0]} lastSeen={friend[2]} />
+        ))}
+    </div>
+  );
+}
+export default FriendsList;
