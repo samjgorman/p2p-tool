40f928c505cedf9c37a00aa7b7c7cd94df8cff53
Sam <samjgorman@gmail.com>
2022-02-24 23:04:03 -0800

    Delete p2p-chat duplicate after new tsconfig settings
    Changed tsconfig includes setting to be more expansive, which may have resulted in a newer version of the p2pchat repo rather than updating the current repo. Therefore, I am deleting the older repository to patch this up and preserve the up to date p2p-chat repo.

--- src/p2p_chat/src/shared/@types/signalhub.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "signalhub" {
-  function signalhub(appName: string, servers: Array<string>): Signalhub;
-  interface Signalhub {
-    subscribe(channel: string): import("stream").Readable;
-    broadcast(channel: string, message: any, callback?: () => void);
-    close(callback?: () => void);
-  }
-  export = signalhub;
-}
